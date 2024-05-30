945c216d3c436248bf548246e1cdc4052f560376
Melina Moraiti <melinamoraith@gmail.com>
2024-05-30 16:02:48 +0300

    Load environment variables from .env files

--- src/setup.sh
+++ src/setup.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
+export $(grep -v '^#' ./master/mysql_master.env | xargs)
+export $(grep -v '^#' ./slave/mysql_slave.env | xargs)
+
 docker-compose down -v
-rm -rf ./master/data/*
-rm -rf ./slave/data/*
+
 docker-compose build
 docker-compose up -d
 
-until docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
-do
-    echo "Waiting for mysql_master database connection..."
-    sleep 4
-done
+wait_for_mysql() {
+    local container_name=$1
+    echo "Waiting for $container_name database connection..."
+    until docker exec $container_name mysql -u root -p${MYSQL_ROOT_PASSWORD} -e ';' &>/dev/null; do
+        sleep 2
+    done
+    echo "$container_name is ready"
+}
 
-priv_stmt='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
-docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
+wait_for_mysql mysql_master
 
-until docker-compose exec mysql_slave sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
-do
-    echo "Waiting for mysql_slave database connection..."
-    sleep 4
-done
+#Create replication user on master
+echo "Creating replication user on master..."
+priv_stmt="CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; GRANT REPLICATION SLAVE ON *.* TO '${MYSQL_USER}'@'%'; FLUSH PRIVILEGES;"
+docker exec mysql_master mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "$priv_stmt"
 
-MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
-CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
-CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
+wait_for_mysql mysql_slave
 
-start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-start_slave_cmd+="$start_slave_stmt"
-start_slave_cmd+='"'
-docker exec mysql_slave sh -c "$start_slave_cmd"
+#Get master status
+MS_STATUS=$(docker exec mysql_master mysql -u root -p${MYSQL_ROOT_PASSWORD} -e 'SHOW MASTER STATUS \G')
+CURRENT_LOG=$(echo "$MS_STATUS" | grep 'File:' | awk '{print $2}')
+CURRENT_POS=$(echo "$MS_STATUS" | grep 'Position:' | awk '{print $2}')
 
-docker exec mysql_slave sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+#CONFIGURE SLAVE
+echo "Configuring the slave..."
+start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='${MYSQL_USER}',MASTER_PASSWORD='${MYSQL_PASSWORD}',MASTER_LOG_FILE='${CURRENT_LOG}',MASTER_LOG_POS=${CURRENT_POS}; START SLAVE;"
+docker exec mysql_slave mysql -u root -p${MYSQL_ROOT_PASSWORD} -e "$start_slave_stmt"
+
+echo "Checking slave status..."
+docker exec mysql_slave mysql -u root -p${MYSQL_ROOT_PASSWORD} -e 'SHOW SLAVE STATUS \G'
+
+echo "MySQL replication setup is complete"
